750d3a48e7b434af10dcc0ce121bc9ed66a3c392
Claire Sky <eclconnolly@gmail.com>
2022-02-11 15:27:29 -0800

    Thought model and Reaction schema set up

---
+++ models/Thought.js
@@ -0,0 +1,56 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema({
+    // set custom id to avoid confustion with parent comment _id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280 
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reations: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
