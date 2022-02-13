00c75af26a42d39d3e46135ea5f9cc1dd093f53f
Claire Sky <eclconnolly@gmail.com>
2022-02-12 20:23:52 -0800

    Route index set up

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -67,7 +67,27 @@ const UserController = {
             res.json(dbUserData);
         })
         .catch(err => res.status(400).json(err));
-    }
+    },
+    // add friend to user
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+          .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $push: { friends: _id } },
+              { new: true }
+            );
+          })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
 };
 
 module.exports = UserController;
