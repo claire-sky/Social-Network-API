fea670159a7e1d888ec3fe87b4a7a2e8ba690b42
Claire Sky <eclconnolly@gmail.com>
2022-02-13 10:00:27 -0800

    Added date formatting to thoughts and reactions

--- models/Thought.js
+++ models/Thought.js
@@ -17,6 +17,7 @@ const ReactionSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+        get: createdAtVal => createdAtVal.toLocaleDateString() + " " + createdAtVal.toLocaleTimeString()
     }
 },
 {
@@ -35,6 +36,7 @@ const ThoughtSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+        get: createdAtVal => createdAtVal.toLocaleDateString() + " " + createdAtVal.toLocaleTimeString()
     },
     username: {
         type: String,
